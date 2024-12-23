defedc27a72afe8b47fbafd7952abc228cbd257c
Dmitry Kandiner <dkandiner@growings.com>
2024-12-23 14:49:54 +0200

    Added the keyboard input port to the MCU. Connected the MCU to the rest of the design.
    Signed-off-by: Dmitry Kandiner <dkandiner@growings.com>

--- src/design/Snake.vhdl
+++ src/design/Snake.vhdl
@@ -47,48 +47,75 @@ end Snake;
 architecture Behavioral of Snake is
     component clk_wiz_0
     port (
-        resetn      : in     std_logic;
-        clk_in1     : in     std_logic;
-        clk_mcu     : out    std_logic;
-        clk_vga     : out    std_logic);
+        resetn  : in  STD_LOGIC;
+        clk_in1 : in  STD_LOGIC;
+        clk_mcu : out STD_LOGIC;
+        clk_vga : out STD_LOGIC);
     end component;
 
     component PS2Receiver is 
         Port (
-            clk        : in STD_LOGIC;
-            kclk       : in STD_LOGIC;
-            kdata      : in STD_LOGIC;
+            clk        : in  STD_LOGIC;
+            kclk       : in  STD_LOGIC;
+            kdata      : in  STD_LOGIC;
             keycodeout : out STD_LOGIC_VECTOR(31 downto 0));
     end component;
 
     component VGA is
         Port (
             -- VGA
-            vga_clk : in STD_LOGIC;
+            vga_clk : in  STD_LOGIC;
             vsync   : out STD_LOGIC;
             hsync   : out STD_LOGIC;
             vga_r   : out STD_LOGIC_VECTOR (3 downto 0);
             vga_g   : out STD_LOGIC_VECTOR (3 downto 0);
             vga_b   : out STD_LOGIC_VECTOR (3 downto 0);
             -- Scren buffer
-            clkb    : IN STD_LOGIC;
-            enb     : IN STD_LOGIC;
-            web     : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-            addrb   : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
-            dinb    : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-            doutb   : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+            clkb    : in  STD_LOGIC;
+            enb     : in  STD_LOGIC;
+            web     : in  STD_LOGIC_VECTOR( 3 DOWNTO 0);
+            addrb   : in  STD_LOGIC_VECTOR( 8 DOWNTO 0);
+            dinb    : in  STD_LOGIC_VECTOR(31 DOWNTO 0);
+            doutb   : out STD_LOGIC_VECTOR(31 DOWNTO 0));
+    end component;
+
+    component mcu_0 is
+        port (
+            Clk              : in  STD_LOGIC;
+            reset_rtl        : in  STD_LOGIC;
+            VMEM_PORT_0_addr : out STD_LOGIC_VECTOR (0 to 31);
+            VMEM_PORT_0_clk  : out STD_LOGIC;
+            VMEM_PORT_0_din  : out STD_LOGIC_VECTOR (0 to 31);
+            VMEM_PORT_0_dout : in  STD_LOGIC_VECTOR (0 to 31);
+            VMEM_PORT_0_en   : out STD_LOGIC;
+            VMEM_PORT_0_rst  : out STD_LOGIC;
+            VMEM_PORT_0_we   : out STD_LOGIC_VECTOR (0 to  3);
+            KBD_IN_0_tri_i   : in  STD_LOGIC_VECTOR (31 downto 0));
     end component;
 
     signal keycodes : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 
+    signal mcu_clock : STD_LOGIC;
     signal vga_clock : STD_LOGIC;
 
+    signal vga_en   : STD_LOGIC;
+    signal vga_we   : STD_LOGIC_VECTOR( 3 downto 0);
+    signal vga_addr : STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
+    signal vga_din  : STD_LOGIC_VECTOR(31 downto 0);
+    signal vga_dout : STD_LOGIC_VECTOR(31 downto 0);
+    
+    alias vmem_addr : STD_LOGIC_VECTOR(vga_addr'reverse_range) is vga_addr;
+    alias vmem_din  : STD_LOGIC_VECTOR(vga_din'reverse_range ) is vga_din;
+    alias vmem_dout : STD_LOGIC_VECTOR(vga_dout'reverse_range) is vga_dout;
+    alias vmem_we   : STD_LOGIC_VECTOR(vga_we'reverse_range  ) is vga_we;
+    
 begin
     clocks : clk_wiz_0
     port map ( 
         clk_in1 => sys_clock,
         resetn  => reset,
-        clk_vga => vga_clock
+        clk_vga => vga_clock,
+        clk_mcu => mcu_clock
     );
     
     kbd : PS2Receiver
@@ -109,11 +136,25 @@ begin
         vga_g   => vga_g,
         vga_b   => vga_b,
         -- Screen buffer
-        clkb    => '0',
-        enb     => '0',
-        web     => (others => '0'),
-        addrb   => (others => 'Z'),
-        dinb    => (others => 'Z')
+        clkb    => mcu_clock,
+        enb     => vga_en,
+        web     => vga_we,
+        addrb   => vga_addr(8 downto 0),
+        dinb    => vga_din,
+        doutb   => vga_dout
+    );
+    
+    mcu : mcu_0
+    port map (
+        Clk              => mcu_clock,
+        reset_rtl        => reset,
+        VMEM_PORT_0_addr => vmem_addr,
+        VMEM_PORT_0_clk  => mcu_clock,
+        VMEM_PORT_0_din  => vmem_din,
+        VMEM_PORT_0_dout => vmem_dout,
+        VMEM_PORT_0_en   => vga_en,
+        VMEM_PORT_0_we   => vmem_we,
+        KBD_IN_0_tri_i   => keycodes
     );
 
 end Behavioral;
