4b9cd52d72f6d781e13ac0634a35b32aa67afb08
Dmitry Kandiner <dmitry.kandiner@gmail.com>
2025-01-13 22:25:22 +0200

    Merged new keyboard driver
    Signed-off-by: Dmitry Kandiner <dmitry.kandiner@gmail.com>

--- fw/Snake/src/snake.c
+++ fw/Snake/src/snake.c
@@ -5,16 +5,40 @@
 #include "screen.h"
 #include "keyboard.h"
 
+#define MAX_SNAKE_LENGTH    (64)
+
+// typedef enum {
+//     dir_up,
+//     dir_right,
+//     dir_down,
+//     dir_left
+// } direction;
+
+typedef struct {
+    uint8_t col;
+    uint8_t row;
+} position;
+
+static struct {
+    uint32_t level;
+    uint32_t score;
+    uint32_t length;
+    position snake[MAX_SNAKE_LENGTH];
+    // direction moving_direction;
+} game = {
+    0
+};
+
+static void init_level(uint32_t level);
+
 int main()
 {
     init_platform();
 
-    scr_clear();
-    scr_print_score(275);
-    scr_print_level(1);
-    scr_draw_border();
+    init_level(game.level);
+//    scr_draw_border();
 
-    kbd_key prev_key = kbd_key_none;
+    kbd_key prev_key = kbd_key_unknown;
     for(;;)
     {
         kbd_key key = kbd_get_last_key();
@@ -22,22 +46,68 @@ int main()
         if (prev_key == key) continue;
         prev_key = key;
 
-        char* key_name;
-        switch (key)
-        {
-            case kbd_key_none:    key_name = "           "; break;
-            case kbd_key_up:      key_name = "up arrow   "; break;
-            case kbd_key_right:   key_name = "right arrow"; break;
-            case kbd_key_down:    key_name = "down arrow "; break;
-            case kbd_key_left:    key_name = "left arrow "; break;
-            case kbd_key_space:   key_name = "space bar  "; break;
-            case kbd_key_escape:  key_name = "escape     "; break;
-            case kbd_key_unknown: key_name = "unknown key"; break;
-        }
-
-        scr_puts(1, 2, key_name);
+        if (key == kbd_key_none) continue;
+        
+        game.level++;
+        init_level(game.level);
     }
 
     cleanup_platform();
     return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+#define MAX_LEVELS      (5)
+static void init_level(uint32_t level)
+{
+    scr_clear();
+    scr_print_score(game.score);
+    scr_print_level(game.level + 1);
+    scr_draw_border();
+    switch (level % MAX_LEVELS)
+    {
+        case 0:
+            break;
+        case 1:
+            for (uint32_t col = 14; col < 25; col++)
+            {
+                scr_putch(col, 14, scr_wall);
+            }
+            break;
+        case 2:
+            for (uint32_t row = 10; row < 19; row++)
+            {
+                scr_putch(14, row, scr_wall);
+                scr_putch(24, row, scr_wall);
+            }
+            break;
+        case 3:
+            for (uint32_t row = 10; row < 19; row++)
+            {
+                scr_putch(14, row, scr_wall);
+                scr_putch(24, row, scr_wall);
+            }
+            for (uint32_t col = 15; col < 24; col++)
+            {
+                scr_putch(col, 14, scr_wall);
+            }
+            break;
+        case 4:
+            for (uint32_t row = 10; row < 13; row++)
+            {
+                scr_putch(14, row, scr_wall);
+                scr_putch(24, row, scr_wall);
+            }
+            for (uint32_t row = 16; row < 19; row++)
+            {
+                scr_putch(14, row, scr_wall);
+                scr_putch(24, row, scr_wall);
+            }
+            for (uint32_t col = 15; col < 24; col++)
+            {
+                scr_putch(col, 10, scr_wall);
+                scr_putch(col, 18, scr_wall);
+            }
+            break;
+    }
 }
